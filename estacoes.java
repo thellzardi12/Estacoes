3248d6377851353f79520c015646d757888addf1
thellzardi12 <130668477+thellzardi12@users.noreply.github.com>
2023-04-14 15:42:51 -0300

    Create estacoes.java

---
+++ estacoes.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.lang.Math;
+
+public class estacoes {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int dia, mes;
+        
+        System.out.println("Informe o mês: ");
+        mes = input.nextInt();
+        System.out.println("Informe o dia: ");
+        dia = input.nextInt();
+        if (mes == 1 || mes == 2 || mes == 3) {
+            if (mes == 3 && dia >= 20) {
+                System.out.println("Outono");
+            } else {
+                System.out.println("Verão");
+            }
+        } else {
+            if (mes == 4 || mes == 5 || mes == 6) {
+                if (mes == 6 && dia >= 20) {
+                    System.out.println("Inverno");
+                } else {
+                    System.out.println("Outono");
+                }
+            } else {
+                if (mes == 7 || mes == 8 || mes == 9) {
+                    if (mes == 9 && dia >= 22) {
+                        System.out.println("Primavera");
+                    } else {
+                        System.out.println("Inverno");
+                    }
+                } else {
+                    if (mes == 10 || mes == 11 || mes == 12) {
+                        if (mes == 12 && dia >= 21) {
+                            System.out.println("Verão");
+                        } else {
+                            System.out.println("Primavera");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
